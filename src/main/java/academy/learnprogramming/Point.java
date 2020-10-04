173a38b5e8104466ccae8841510ca7d42d766418
SrinivasaChakka <Srinivasa.Chakka@tesco.com>
2020-10-04 21:31:16 +0530

    Learning Constructors

---
+++ src/main/java/academy/learnprogramming/Point.java
@@ -0,0 +1,44 @@
+package academy.learnprogramming;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(){
+        return(distance(0.0,0.0));
+    }
+
+    public double distance(int x,int y){
+        return( Math.sqrt((this.x-x)*(this.x-x)+(this.y-y)*(this.y-y)));
+    }
+
+    public double distance(Point second){
+        return(distance(second.getX(),second.getY()));
+    }
+
+}
